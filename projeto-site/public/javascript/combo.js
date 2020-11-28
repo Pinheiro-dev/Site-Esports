9c3f625d84d4ec80c4c95573252cfe23799a5163
Pinheiro-dev <matheus.barbosa@bandtec.com.br>
2020-11-28 10:53:31 -0300

    index-valorant finalizado

---
+++ projeto-site/public/javascript/combo.js
@@ -0,0 +1,13 @@
+// Função para alterar de página
+function trocar_pagina() {
+
+    if (lista_jogos.value == "valorant") {
+        window.location.href = "index-valorant.html";
+    } else if (lista_jogos.value == "lol") {
+        window.location.href = "index-lol.html";
+    } else if (lista_jogos.value == "cs") {
+        window.location.href = "index-cs.html";
+    } else {
+        alert ("Escolha uma das opções abaixo");
+    }
+}
