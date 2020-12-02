deb8295f642b929a1727b651890c6b382e33eae4
Pinheiro-dev <matheus.barbosa@bandtec.com.br>
2020-12-02 10:53:39 -0300

    atualização

--- projeto-site/config/config.js
+++ projeto-site/config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   production: {
-    username: 'bdEsports',
-    password: '#Gf47240958817',
+    username: 'bdEsports', 
+    password: 'senha',
     database: 'bdEsports',
     host: 'bdesports.database.windows.net',
     dialect: 'mssql',
